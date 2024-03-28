87e49d482714101e64a99bc8b19290d345e2fe81
Mikołaj Kuranowski <mkuranowski@gmail.com>
2024-03-28 11:41:36 +0100

    Simple simulator: basic turtle positioning

--- src/simulator/simple.py
+++ src/simulator/simple.py
@@ -1,29 +1,38 @@
-from typing import List
+from math import cos, sin
+from typing import Dict, List
 
 from .simulator import CameraCell, ColorChecker, Position, Simulator
 
 
 class SimpleSimulator(Simulator):
     def __init__(self) -> None:
-        pass
+        self.turtles: Dict[str, Position] = {}
 
     def has_turtle(self, name: str) -> bool:
-        raise NotImplementedError
+        return name in self.turtles
 
     def kill_turtle(self, name: str) -> None:
-        raise NotImplementedError
+        del self.turtles[name]
 
-    def spawn_turtle(self, name: str, at: Position = ...) -> None:
-        raise NotImplementedError
+    def spawn_turtle(self, name: str, at: Position = Position()) -> None:
+        if name in self.turtles:
+            raise KeyError(name)
+        self.turtles[name] = at
 
     def set_position(self, name: str, new_pos: Position) -> None:
-        raise NotImplementedError
+        if name not in self.turtles:
+            raise KeyError(name)
+        self.turtles[name] = new_pos
 
     def move(self, name: str, distance: float, angle: float) -> None:
-        raise NotImplementedError
+        current_pos = self.turtles[name]
+        new_angle = current_pos.angle + angle
+        new_x = distance * cos(new_angle)
+        new_y = distance * sin(new_angle)
+        self.turtles[name] = Position(new_x, new_y, new_angle)
 
     def get_position(self, name: str) -> Position:
-        raise NotImplementedError
+        return self.turtles[name]
 
     def read_camera(
         self,
