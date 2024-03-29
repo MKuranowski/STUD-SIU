b3d29e975ba5755ba24699f80a59f4a74bf908fa
Mikołaj Kuranowski <mkuranowski@gmail.com>
2024-03-29 01:02:05 +0100

    Simple simulator: color checking

--- src/simulator/simple.py
+++ src/simulator/simple.py
@@ -1,12 +1,35 @@
 from math import cos, sin
-from typing import Dict, List
+from typing import Dict, List, Tuple, TypeVar
+
+import cv2
+
+from src.simulator.simulator import Color
 
 from .simulator import CameraCell, ColorChecker, Position, Simulator
 
+Number = TypeVar("Number", int, float)
+
 
 class SimpleSimulator(Simulator):
-    def __init__(self) -> None:
+    def __init__(self, background_path: str = "roads.png", scale_m_to_px: float = 22.0) -> None:
+        self.scale_m_to_px: float = scale_m_to_px
+        self.background_path = background_path
+
+        self.background = cv2.imread(self.background_path)
+        """A (width, height, 3)-shaped numpy matrix of 0-255 uint8 representing
+        the blue, green and red channel values"""
+
         self.turtles: Dict[str, Position] = {}
+
+    def position_to_pixels(self, p: Position) -> Tuple[int, int]:
+        return (
+            clamp(int(p.x * self.scale_m_to_px), 0, self.background.shape[0]),
+            clamp(
+                self.background.shape[1] - int(p.y * self.scale_m_to_px),
+                0,
+                self.background.shape[1],
+            ),
+        )
 
     def has_turtle(self, name: str) -> bool:
         return name in self.turtles
@@ -44,4 +67,28 @@ class SimpleSimulator(Simulator):
         raise NotImplementedError
 
     def get_color_checker(self, name: str) -> ColorChecker:
-        raise NotImplementedError
+        return SimpleColorChecker(self, name)
+
+
+class SimpleColorChecker(ColorChecker):
+    def __init__(self, simple_simulator: SimpleSimulator, name: str) -> None:
+        self.simple_simulator = simple_simulator
+        self.name = name
+
+    def check(self) -> Color:
+        position = self.simple_simulator.get_position(self.name)
+        x, y = self.simple_simulator.position_to_pixels(position)
+        b, g, r = self.simple_simulator.background[x, y]
+        return Color(
+            r=clamp((r - 200) / 50, -1.0, 1.0),
+            g=clamp(g / 255, 0.0, 1.0),
+            b=clamp((b - 200) / 50, -1.0, 1.0),
+        )
+
+
+def clamp(x: Number, lo: Number, hi: Number) -> Number:
+    if x < lo:
+        return lo
+    if x > hi:
+        return hi
+    return x
