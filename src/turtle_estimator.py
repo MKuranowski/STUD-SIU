4f1695fbdd9035bb7b03226f50b183ddadf5ef40
Mikołaj Kuranowski <mkuranowski@gmail.com>
2024-04-18 17:18:05 +0200

    turtle_estimator: fix imports

--- src/turtle_estimator.py
+++ src/turtle_estimator.py
@@ -1,18 +1,20 @@
 # pyright: basic
 
-import logging
 import dataclasses
+import logging
+from multiprocessing.pool import Pool
 from pathlib import Path
 from random import Random
 from typing import NamedTuple
-from multiprocessing.pool import Pool
 
 import numpy as np
 import numpy.typing as npt
 
-from .env_base import Parameters
 from .dqn_single import DQNParameters
+from .env_base import Parameters
+from .env_single import EnvSingle
 from .play_single import PlaySingle
+from .simulator import create_simulator
 
 MODELS_DIR = Path("models")
 
@@ -34,20 +36,14 @@ def multithreaded_train(args: tuple[Parameters, DQNParameters]) -> ModelResult:
 
 def train(parameters: Parameters, dqn_parameters: DQNParameters) -> ModelResult:
     with create_simulator() as simulator:
-        env = EnvSingle(
-            simulator,
-            parameters=parameters
-        )
+        env = EnvSingle(simulator, parameters=parameters)
         env.setup("routes.csv", agent_limit=1)
         turtle_name = next(iter(env.agents))
-        model = PlaySingle(
-            env,
-            parameters=dqn_parameters
-        )
+        model = PlaySingle(env, parameters=dqn_parameters)
         model.train(turtle_name, randomize_section=True)
         model.random = Random(0)
         restricted_parameters = dataclasses.asdict(parameters)
-        restricted_parameters['max_steps'] = 4_000
+        restricted_parameters["max_steps"] = 4_000
         model.env.parameters = Parameters(**restricted_parameters)
         env.reset()
         reward = model.play_until_crash(max_laps=4)
@@ -64,9 +60,6 @@ if __name__ == "__main__":
 
     import coloredlogs
 
-    from .env_single import EnvSingle
-    from .simulator import create_simulator
-
     arg_parser = ArgumentParser()
     arg_parser.add_argument("-v", "--verbose", action="store_true", help="enable debug logging")
     args = arg_parser.parse_args()
@@ -78,26 +71,26 @@ if __name__ == "__main__":
     max_episodes = 100
 
     parameters_distributions = {
-        'grid_res': range(4, 10),
-        'cam_res': [200],
-        'reward_forward_rate': [0.5, 1.0, 2.0, 4.0, 8.0],
-        'reward_reverse_rate': [-10.0, -15.0, -20],
-        'reward_speeding_rate': [-10.0, -15.0, -20.0],
-        'reward_distance_rate': [2, 4, 8, 16],
-        'out_of_track_fine': [-10.0, -15.0, -20.0],
-        'max_steps': [10, 20, 40, 80],
-        'goal_radius': [0.25, 0.5, 1.0, 1.5, 2.0],
+        "grid_res": range(4, 10),
+        "cam_res": [200],
+        "reward_forward_rate": [0.5, 1.0, 2.0, 4.0, 8.0],
+        "reward_reverse_rate": [-10.0, -15.0, -20],
+        "reward_speeding_rate": [-10.0, -15.0, -20.0],
+        "reward_distance_rate": [2, 4, 8, 16],
+        "out_of_track_fine": [-10.0, -15.0, -20.0],
+        "max_steps": [10, 20, 40, 80],
+        "goal_radius": [0.25, 0.5, 1.0, 1.5, 2.0],
     }
     dqn_parameters_distributions = {
-        'discount': [0.9],
-        'replay_memory_max_size': [20_000],
-        'replay_memory_min_size': [4_000],
-        'minibatch_size': [32],
-        'training_batch_divisor': [4],
-        'target_update_period': [20],
-        'max_episodes': [max_episodes],
-        'train_period': [4],
-        'save_period': [max_episodes],
+        "discount": [0.9],
+        "replay_memory_max_size": [20_000],
+        "replay_memory_min_size": [4_000],
+        "minibatch_size": [32],
+        "training_batch_divisor": [4],
+        "target_update_period": [20],
+        "max_episodes": [max_episodes],
+        "train_period": [4],
+        "save_period": [max_episodes],
     }
 
     random = Random(seed)
@@ -105,17 +98,24 @@ if __name__ == "__main__":
     results: list[ModelResult] = []
 
     parameters_from_distributions = [
-        Parameters(**{key: random.choice(values) for key, values in parameters_distributions.items()})
+        Parameters(
+            **{key: random.choice(values) for key, values in parameters_distributions.items()}
+        )
         for _ in range(iterations)
     ]
 
     dqn_parameters_from_distributions = [
-        DQNParameters(**{key: random.choice(values) for key, values in dqn_parameters_distributions.items()})
+        DQNParameters(
+            **{key: random.choice(values) for key, values in dqn_parameters_distributions.items()}
+        )
         for _ in range(iterations)
     ]
 
     with Pool() as pool:
-        results = pool.map(multithreaded_train, zip(parameters_from_distributions, dqn_parameters_from_distributions))
+        results = pool.map(
+            multithreaded_train,
+            zip(parameters_from_distributions, dqn_parameters_from_distributions),
+        )
 
     for i, result in enumerate(sorted(results, key=lambda x: x.reward, reverse=True), start=1):
-        logger.info('%d) %f %s', i, result.reward, result.signature)
+        logger.info("%d) %f %s", i, result.reward, result.signature)
