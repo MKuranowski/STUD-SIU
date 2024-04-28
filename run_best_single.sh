52fe2ba8d5f91df724ac99395c7fe04ea688f739
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2024-04-28 22:30:29 +0200

    Add full name to run_best_single

--- run_best_single.sh
+++ run_best_single.sh
@@ -1,1 +1,1 @@
-./do_run.sh -m src.play_single models/dqn-7704f2*
+./do_run.sh -m src.play_single models/dqns-7704f2-Gr7_Cr200_Sw4.0_Sv-20_Sf-15.0_Dr4_Oo-20.0_Ms20_Pb6_D0.85_M20000_m4000_B32_U20_T4.h5
