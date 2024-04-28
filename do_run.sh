07ed839eadc7a5dd36ede0871209128d2da86e26
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2024-04-28 21:49:49 +0200

    Update do_run script

--- do_run.sh
+++ do_run.sh
@@ -2,7 +2,11 @@
 set -ex
 export ROS_HOSTNAME=localhost
 . /opt/ros/noetic/setup.bash
-. /opt/siu_ws/devel/setup.bash
+if [ -f "/opt/siu_ws/devel/setup.bash" ]; then
+    . /opt/siu_ws/devel/setup.bash
+else
+    . /root/siu_ws/devel/setup.bash
+fi
 roslaunch -v turtlesim siu.launch &
 ROS_PID="$!"
 trap 'kill $ROS_PID; wait $ROS_PID' EXIT
