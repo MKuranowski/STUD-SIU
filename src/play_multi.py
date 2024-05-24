558abe855af26f743dcc6fa6cf05f533ed422b8b
Mikołaj Kuranowski <mkuranowski@gmail.com>
2024-05-24 15:41:02 +0000

    PlayMulti: remove crashed turtles from environment

--- src/play_multi.py
+++ src/play_multi.py
@@ -81,6 +81,7 @@ class PlayMulti(DQNMulti):
             # Remove crashed turtles
             for name in turtles_to_remove:
                 self.env.simulator.kill_turtle(name)
+                del self.env.agents[name]
                 crashed_episodes[name] = active_episodes.pop(name)
 
         return sum(
